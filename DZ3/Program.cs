388a56b8f6881b3062127b32a7f429b1df56c7c5
Sergey <sergeyfedorenko.a89@gmail.com>
2022-08-31 18:34:21 +0300

    DZ3 zadacha2

--- DZ3/Program.cs
+++ DZ3/Program.cs
@@ -20,3 +20,20 @@
 Console.WriteLine ("Введите число ");
 int drom = Convert.ToInt32(Console.ReadLine());
 Pali (drom);*/
+
+/*Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.*/
+
+/*double Distance(double x1, double y1, double z1, double x2, double y2, double z2)
+{
+    return Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1) + (z2-z1) * (z2-z1));
+}
+Console.WriteLine("Введите координаты первых точек (x, y, z) : ");
+double xA = Convert.ToDouble(Console.ReadLine());
+double yA = Convert.ToDouble(Console.ReadLine());
+double zA = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("ВВедите координаты вторых точек (x, y, z) : ");
+double xB = Convert.ToDouble(Console.ReadLine());
+double yB = Convert.ToDouble(Console.ReadLine());
+double zB = Convert.ToDouble(Console.ReadLine());
+double Dis = Distance(xA, yA, zA, xB, yB, zB);
+Console.WriteLine($"3D {Dis}");*/
