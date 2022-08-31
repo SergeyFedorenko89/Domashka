8127654fbcaacd5e304547cf632b54785a76a709
Sergey <sergeyfedorenko.a89@gmail.com>
2022-08-31 18:31:00 +0300

    DZ3 Pol

---
+++ DZ3/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.*/
+
+/*void Pali (int number)
+{
+    int revers = 0;
+    int num = number;
+    int ber = 0;
+    while (number > 0)
+     {
+        ber = number % 10;
+        number = number / 10;
+        revers = revers * 10;
+        revers = revers + ber;
+     }
+    if (num == revers) 
+    Console.WriteLine ($"Это число палиндром");
+    else
+    Console.WriteLine($"Это число не палиндром");
+}
+Console.WriteLine ("Введите число ");
+int drom = Convert.ToInt32(Console.ReadLine());
+Pali (drom);*/
