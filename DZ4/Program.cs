70714faab3600ce7d346278fd98266a9d7a0c1d9
Sergey <sergeyfedorenko.a89@gmail.com>
2022-09-04 17:19:58 +0300

    DZ4 zadacha1

---
+++ DZ4/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B*/
+
+int AiB (int number1, int number2)
+{
+    int current = 1;
+    for (int i = 0; i < number2; i = i + 1 )
+    {
+        current = current * number1;
+    }
+    return current;
+}
+Console.WriteLine ("Введите первое число ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите второе число ");
+int B = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(AiB (A , B));
