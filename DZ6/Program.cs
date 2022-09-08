ea4d3570727b8bd444ed1bd185a2335ebb23846f
Sergey <sergeyfedorenko.a89@gmail.com>
2022-09-08 15:52:41 +0300

    DZ6 zadacha2

--- DZ6/Program.cs
+++ DZ6/Program.cs
@@ -1,6 +1,6 @@
 ﻿/*Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь*/
 
-int enk(int num)
+/*int enk(int num)
 {
     int plus = 0;
     for(int i = 0; i < num; i = i + 1)
@@ -14,4 +14,28 @@ int enk(int num)
 }
 Console.WriteLine("Введите количество чисел ");
 int num = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine ($"Положительных чисел {enk(num)} ");
+Console.WriteLine ($"Положительных чисел {enk(num)} ");*/
+
+
+/*Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем*/
+
+
+/*void enk (double b1, double k1, double b2, double k2)
+{
+    if (k2 - k1 != 0)
+    {
+        double x = (b1 - b2) / (k2 - k1);
+        double y = (k2 * b1 - k1 * b2) / (k2 - k1);
+        Console.WriteLine($"Точка пересечения {x} и {y}");
+    }
+    else Console.WriteLine ("Не пересекаются ");
+}
+Console.WriteLine ("Введите b1 ");
+int newnum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите k1 ");
+int newnumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите b2 ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите k2 ");
+int num = Convert.ToInt32(Console.ReadLine());
+enk (newnum, newnumber, number, num);*/
