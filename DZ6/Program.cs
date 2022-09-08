191291c818cf8341dd1a1562b55d399f2549753e
Sergey <sergeyfedorenko.a89@gmail.com>
2022-09-08 12:06:12 +0300

    DZ6 zadacha1

---
+++ DZ6/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь*/
+
+int enk(int num)
+{
+    int plus = 0;
+    for(int i = 0; i < num; i = i + 1)
+    {
+      Console.WriteLine($"Введите число {i + 1}: ");
+      int number = Convert.ToInt32(Console.ReadLine());
+      if(number > 0)
+      plus = plus + 1;
+    }
+    return plus;
+}
+Console.WriteLine("Введите количество чисел ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ($"Положительных чисел {enk(num)} ");
