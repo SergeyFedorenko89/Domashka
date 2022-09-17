b45fec522c634f9b8fa48aa6c336adc318613a9c
Sergey <sergeyfedorenko.a89@gmail.com>
2022-09-18 01:24:29 +0300

    DZ8 zadacha2

--- DZ8/Program.cs
+++ DZ8/Program.cs
@@ -1,7 +1,7 @@
 ﻿/*Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
 Например, задан массив*/
 
-int[,] NewRandomArray(int row, int col, int min, int max)
+/*int[,] NewRandomArray(int row, int col, int min, int max)
 {
      int[,] array = new int[row, col];
      for(int i = 0; i < row; i++)
@@ -51,4 +51,61 @@ int[,] array = NewRandomArray(row, col, min, max);
 Show2Array(array);
 Console.WriteLine();
 Enk(array);
-Show2Array(array);
+Show2Array(array);*/
+
+
+/*Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+Например, задан массив*/
+
+/*int[,] Random2Array()
+{
+    Console.Write("Введите количество строк: ");
+    int row = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите количество столбцов: ");
+    int col = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите минимально число: ");
+    int min = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите максимально число: ");
+    int max = Convert.ToInt32(Console.ReadLine());
+    int[,] newarray = new int[row, col];
+    for(int i = 0; i < row; i++)
+    {
+        for(int j = 0; j < col; j++)
+        {
+            newarray[i,j] = new Random().Next(min, max + 1);
+        }    
+    }
+    return newarray;
+}
+void EnkArray2(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        Console.Write(array[i,j] + " ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+void EnkMin(int[,] array) 
+{
+    int row = 0;
+    int min = 0; 
+    for(int j = 0; j < array.GetLength(1); j++)
+    min = array[0, j] + min;   
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        int sum = 0;
+        for(int j = 0; j < array.GetLength(1); j++)
+        sum = array[i,j] + sum;
+    if(sum < min)
+      {
+        min = sum;
+        row = i;
+      }
+    }
+    Console.Write($"Наименьшая сумма элементов в строке номер {row + 1}");
+}
+int[,] Array = Random2Array();
+EnkArray2(Array);
+EnkMin(Array);*/
