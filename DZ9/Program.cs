5de2516a4806908e01710312ddba8a911e8abf1a
Sergey <sergeyfedorenko.a89@gmail.com>
2022-09-19 21:28:51 +0300

    DZ9 zadacha2

---
+++ DZ9/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N*/
+
+/*int Sum(int m, int n)
+{
+    if(n < m)
+    {
+        if(m > n) return Sum(m - 1, n) + m;
+        else return m;
+    }
+    else
+    {        
+        if(n > m) return Sum(m, n - 1) + n;
+        else return n;
+    }
+}
+Console.Write("Введите первое число: ");
+int onenumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int twonumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+if(onenumber < twonumber) 
+    Console.WriteLine($"Сумма натуральных элементов от {onenumber} до {twonumber} равна {Sum(onenumber, twonumber)}");
+else
+    Console.WriteLine($"Сумма натуральных элементов от {onenumber} до {twonumber} равна {Sum(onenumber, twonumber)}");*/
